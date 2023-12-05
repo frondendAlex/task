c293ee7e46f9d32c10b38ec26fca0382baa13964
frondendAlex <61823718+frondendAlex@users.noreply.github.com>
2023-12-05 12:12:54 +0300

    Не большая структура для контента

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     
-    <header class="section-header">
+    <header class="header">
         <div class="container">
             <div class="header">
                 <div class="header__top"></div>
@@ -17,13 +17,63 @@
         </div>
     </header>
 
-    <section class="section-one">
+    <section class="section-content">
         <div class="container">
             <div class="section__header">
                 <h1 class="section__title">Отчет</h1>
             </div>
+            <div class="content">
+                <div class="content__wrap">
+
+                    <div class="content__col">
+                        <div class="content__item">
+                            <div class="content__number">
+                                <span>№1</span>
+                            </div>
+                            <div class="content__title">
+                                <h2>Отчет1</h2>
+                            </div>
+                            <div class="content__link">
+                                <a href="">http://192.168.1.99/demo8/dist/apps/user-management/users/list.html</a>
+                            </div>
+                            <div class="content__count">
+                                <span class="content__count-text">Кол-во:</span>
+                                <span class="content__count-number">1</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="content__col">
+                        <div class="content__item">
+                            <div class="content__number">
+                                <span>№2</span>
+                            </div>
+                            <div class="content__title">
+                                <h2>Отчет2</h2>
+                            </div>
+                            <div class="content__link">
+                                <a href="">Ссылка2</a>
+                            </div>
+                            <div class="content__count">
+                                <span class="content__count-text">Кол-во:</span>
+                                <span class="content__count-number">3</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="content__total">
+                    Итого: <span>4</span>
+                </div>
+            </div>
         </div>
     </section>
+
+
+    <footer class="footer">
+        
+    </footer>
 
     <script src="js/main.js"></script>
 </body>
