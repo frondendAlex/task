74c8083a17114e20c895e281805ef95bca3a2806
frondendAlex <alex-moskow@list.ru>
2023-12-08 09:26:12 +0300

    Таблица временная

---
+++ admin/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa quod commodi tenetur, dolor magni quos iusto praesentium ratione inventore, ipsam sequi. Nihil incidunt tenetur blanditiis. Ipsum voluptatum alias eaque saepe, perspiciatis consectetur tenetur magnam, necessitatibus suscipit placeat debitis. Id adipisci tempore ratione vero modi, necessitatibus voluptate doloremque minima ipsam laudantium, incidunt corporis esse quos eos iste dignissimos a facere accusamus distinctio quae atque? Architecto, eos provident eaque velit consequuntur expedita consequatur illum, maiores vero blanditiis doloremque cupiditate quas maxime dolor repellat harum? Similique tempora labore soluta cupiditate id, rerum tempore aliquid, excepturi, cum laborum sunt. Aut tempore autem nisi tempora dignissimos animi labore ratione voluptatem. Minima harum explicabo ex rem a soluta modi neque accusantium cupiditate quo beatae, cum consequuntur voluptates laborum magnam dolorem perferendis et repudiandae quod autem. Consectetur obcaecati corporis tempore cupiditate iusto nulla assumenda accusamus itaque doloribus facere, nemo, quia expedita vitae vel sint totam pariatur harum deleniti dolorum, excepturi saepe. Laborum eveniet dolorem est nesciunt quia? Similique illum laudantium totam earum magni velit itaque consequatur modi et dolore autem praesentium perferendis, aut ad deleniti debitis nostrum labore, veniam cumque culpa tempora assumenda sapiente. Mollitia a excepturi quas assumenda optio dicta libero quibusdam vitae voluptates quod exercitationem cum enim, voluptatibus praesentium. Vitae, sapiente neque dolorum reprehenderit ullam tempore obcaecati animi architecto ea voluptates, ipsum provident consequatur eveniet harum unde perferendis omnis non consectetur! Autem sed eaque illo ducimus, similique voluptas nemo voluptatem ratione perspiciatis facere possimus dolorem suscipit, inventore et neque odit eveniet corrupti sapiente. Tempore sequi perferendis dignissimos aut tenetur! Placeat distinctio minima obcaecati nesciunt sed harum adipisci quo est fugit vero possimus libero expedita, modi dicta aspernatur sunt aut earum voluptatem molestias suscipit dolores autem. Doloremque exercitationem suscipit ad veniam sunt, modi consequatur ratione numquam necessitatibus possimus itaque dolores! Illum quae aspernatur in sit unde animi, nesciunt iure voluptas corporis aperiam neque placeat at reiciendis exercitationem molestiae similique voluptatem dolores assumenda reprehenderit recusandae cum temporibus. Sit laudantium quam obcaecati, voluptas aperiam nobis omnis vitae illum id minus iusto ullam dolore quaerat autem. Voluptatibus maiores error nulla est necessitatibus? Laudantium officiis aspernatur voluptatem sapiente magnam assumenda magni porro optio omnis mollitia enim delectus hic necessitatibus tenetur suscipit id culpa ea saepe exercitationem, fugiat voluptatibus possimus dolorum modi quisquam. Atque a similique soluta autem expedita dolorem error architecto, nobis, obcaecati eos voluptatibus voluptates deserunt assumenda nisi pariatur! Laudantium aliquam nobis, eum reprehenderit aliquid provident ducimus quis voluptatum distinctio nam, doloremque porro molestiae. Veniam autem tenetur ab perferendis iste, esse illo nam perspiciatis adipisci, animi doloribus nobis, totam praesentium! Explicabo deleniti sint impedit mollitia possimus quo veritatis minima, aspernatur, dolorum soluta fugit quia hic eaque nulla, atque ipsum minus sit laborum dolorem iure esse id doloremque eos accusantium. Aperiam assumenda reiciendis nostrum dolores qui possimus ipsum, cum itaque laborum natus vero esse eius molestiae tempora dignissimos maiores! Suscipit, perferendis. Facere, itaque! Voluptates velit qui explicabo? Omnis dolore officiis nostrum saepe consequuntur labore debitis, mollitia libero quae illum doloribus distinctio eius inventore voluptatem necessitatibus cum nihil, quo, odio temporibus.</p>
+    </div>
+
+    <div class="container">
+        <div class="tables">
+            <table>
+                <thead>
+                    <tr>
+                        <th class="th-1">ID</th>
+                        <th class="th-2">Ячейка 1</th>
+                        <th class="th-3">Ячейка 2</th>
+                        <th class="th-4">Ячейка 3</th>
+                        <th class="th-5">Ячейка 4</th>
+                        <th class="th-6">Ячейка 5</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Запись 1</td>
+                        <td>Запись 2</td>
+                        <td>Запись 3</td>
+                        <td>Запись 4</td>
+                        <td>Запись 5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="container">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa quod commodi tenetur, dolor magni quos iusto praesentium ratione inventore, ipsam sequi. Nihil incidunt tenetur blanditiis. Ipsum voluptatum alias eaque saepe, perspiciatis consectetur tenetur magnam, necessitatibus suscipit placeat debitis. Id adipisci tempore ratione vero modi, necessitatibus voluptate doloremque minima ipsam laudantium, incidunt corporis esse quos eos iste dignissimos a facere accusamus distinctio quae atque? Architecto, eos provident eaque velit consequuntur expedita consequatur illum, maiores vero blanditiis doloremque cupiditate quas maxime dolor repellat harum? Similique tempora labore soluta cupiditate id, rerum tempore aliquid, excepturi, cum laborum sunt. Aut tempore autem nisi tempora dignissimos animi labore ratione voluptatem. Minima harum explicabo ex rem a soluta modi neque accusantium cupiditate quo beatae, cum consequuntur voluptates laborum magnam dolorem perferendis et repudiandae quod autem. Consectetur obcaecati corporis tempore cupiditate iusto nulla assumenda accusamus itaque doloribus facere, nemo, quia expedita vitae vel sint totam pariatur harum deleniti dolorum, excepturi saepe. Laborum eveniet dolorem est nesciunt quia? Similique illum laudantium totam earum magni velit itaque consequatur modi et dolore autem praesentium perferendis, aut ad deleniti debitis nostrum labore, veniam cumque culpa tempora assumenda sapiente. Mollitia a excepturi quas assumenda optio dicta libero quibusdam vitae voluptates quod exercitationem cum enim, voluptatibus praesentium. Vitae, sapiente neque dolorum reprehenderit ullam tempore obcaecati animi architecto ea voluptates, ipsum provident consequatur eveniet harum unde perferendis omnis non consectetur! Autem sed eaque illo ducimus, similique voluptas nemo voluptatem ratione perspiciatis facere possimus dolorem suscipit, inventore et neque odit eveniet corrupti sapiente. Tempore sequi perferendis dignissimos aut tenetur! Placeat distinctio minima obcaecati nesciunt sed harum adipisci quo est fugit vero possimus libero expedita, modi dicta aspernatur sunt aut earum voluptatem molestias suscipit dolores autem. Doloremque exercitationem suscipit ad veniam sunt, modi consequatur ratione numquam necessitatibus possimus itaque dolores! Illum quae aspernatur in sit unde animi, nesciunt iure voluptas corporis aperiam neque placeat at reiciendis exercitationem molestiae similique voluptatem dolores assumenda reprehenderit recusandae cum temporibus. Sit laudantium quam obcaecati, voluptas aperiam nobis omnis vitae illum id minus iusto ullam dolore quaerat autem. Voluptatibus maiores error nulla est necessitatibus? Laudantium officiis aspernatur voluptatem sapiente magnam assumenda magni porro optio omnis mollitia enim delectus hic necessitatibus tenetur suscipit id culpa ea saepe exercitationem, fugiat voluptatibus possimus dolorum modi quisquam. Atque a similique soluta autem expedita dolorem error architecto, nobis, obcaecati eos voluptatibus voluptates deserunt assumenda nisi pariatur! Laudantium aliquam nobis, eum reprehenderit aliquid provident ducimus quis voluptatum distinctio nam, doloremque porro molestiae. Veniam autem tenetur ab perferendis iste, esse illo nam perspiciatis adipisci, animi doloribus nobis, totam praesentium! Explicabo deleniti sint impedit mollitia possimus quo veritatis minima, aspernatur, dolorum soluta fugit quia hic eaque nulla, atque ipsum minus sit laborum dolorem iure esse id doloremque eos accusantium. Aperiam assumenda reiciendis nostrum dolores qui possimus ipsum, cum itaque laborum natus vero esse eius molestiae tempora dignissimos maiores! Suscipit, perferendis. Facere, itaque! Voluptates velit qui explicabo? Omnis dolore officiis nostrum saepe consequuntur labore debitis, mollitia libero quae illum doloribus distinctio eius inventore voluptatem necessitatibus cum nihil, quo, odio temporibus.</p>
+    </div>
+    
+</body>
+</html>
