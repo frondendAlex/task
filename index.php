230a5e652b2c46f1bae78d83d672c4b52f0ef697
frondendAlex <61823718+frondendAlex@users.noreply.github.com>
2024-05-22 17:46:02 +0300

    Проба test1

--- index.php
+++ index.php
@@ -41,6 +41,8 @@
 
             <h1>Создана ветка test</h1>
             <h1>Коммит 1</h1>
+            <h1>Создана ветка test1</h1>
+            <p>Переместил из ветки test1 в test</p>
 
             <div class="content">
                 <div class="content__item content--active" data-content="1">
